e362b34f3747ad617d97bfc1f0198c81d1f95e13
phucle158 <phucle1582003@gmail.com>
2025-04-11 22:10:44 +0700

    Fix: remove root in vite config for Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default () => {
-  return defineConfig({
-    root: "./src",
-    base: "",
-    plugins: [react()],
-  });
-};
+// ✅ Cấu hình đúng với index.html ở thư mục gốc
+export default defineConfig({
+  plugins: [react()],
+});
